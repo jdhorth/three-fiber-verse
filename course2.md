d968f08cc7cfaf511a20823ccf36bb926a6e9d69
Justin Horth <jdhorth@gmail.com>
2022-09-26 21:29:24 -0400

    course2 orbit controls

---
+++ course2.md
@@ -0,0 +1,7 @@
+## Orbit Controls
+## Course 2
+
+- Course 2
+- Orbit controls
+- Used @react-three-drei instead to handle Orbit Controls
+- Install the package instead of usethree
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,27 @@
 import type { NextPage } from 'next'
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useThree } from '@react-three/fiber'
 import AnimatedBox from '../components/AnimatedBox'
+import { OrbitControls } from "@react-three/drei"
+import { useEffect } from 'react'
 
+// const CameraOrbitController = () => {
+//   const { camera, gl } = useThree();
 
+//   useEffect(() => {
+//     const controls = new OrbitControls(camera, gl.domElement);
+//     return () => {
+//       controls.dispose();
+//     }
+//   }, [camera, gl]);
+//   return null;
+// }
 
 
 const Home: NextPage = () => {
   return (
     <div className="container" >
       <Canvas>
+        <OrbitControls />
         <ambientLight intensity={0.1} />
         <directionalLight color="red" position={[0, 0, 5]} />
         <AnimatedBox />
